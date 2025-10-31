0fdd2a7aee1352047ba1e7037c1b8b7d6a1bda5c
j2151348-sketch <j2151348@gmail.com>
2025-10-31 13:44:26 +0100

    Ajout du fichier src/App.jsx de base pour le projet React + Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur le site de révision</h1>
+      <p>Commencez à réviser vos sujets préférés !</p>
+    </div>
+  );
+}
+
+export default App;
